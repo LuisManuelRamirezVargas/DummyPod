c8660d5dffa855d7c4ec042fcb1b77998a4507d8
Luis Manuel Ramirez Vargas <luis_manuel.ramirez_vargas@syngenta.com>
2021-03-09 15:13:44 -0600

    Initial Commit

---
+++ DummyPod/DummyPod.h
@@ -0,0 +1,18 @@
+//
+//  DummyPod.h
+//  DummyPod
+//
+//  Created by Ramirez, Luis Manuel on 09/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DummyPod.
+FOUNDATION_EXPORT double DummyPodVersionNumber;
+
+//! Project version string for DummyPod.
+FOUNDATION_EXPORT const unsigned char DummyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyPod/PublicHeader.h>
+
+
