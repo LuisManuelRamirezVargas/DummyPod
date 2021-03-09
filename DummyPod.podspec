8900a909dead62677357e1c16ca6e4c5b3c1da35
Luis Manuel Ramirez Vargas <luis_manuel.ramirez_vargas@syngenta.com>
2021-03-09 16:35:40 -0600

    Initial Commit

---
+++ DummyPod.podspec
@@ -0,0 +1,24 @@
+Pod::Spec.new do |s|
+
+  s.platform = :ios
+  s.ios.deployment_target = '11.0'
+  s.name = "DummyPod"
+  s.summary = "DummyPod is a test pod"
+  s.requires_arc = true
+  
+  s.version = "0.1.0"
+  
+  s.license = { :type => "MIT", :file => "LICENSE" }
+  
+  s.author = { "Luis Manuel Ramirez Vargas" => "luis.manuel.ramirez.91@gmail.com" }
+  
+  s.homepage = "https://github.com/LuisMDeveloper/DummyPod"
+  
+  s.source = { :git => "https://github.com/LuisMDeveloper/DummyPod.git", 
+               :tag => "#{s.version}" }
+
+  s.source_files = "DummyPod/**/*.{swift}"
+  s.resources = "DummyPod/**/*.{png,jpeg,jpg,storyboard,xib,xcassets}"
+  s.swift_version = "4.2"
+  
+end
---
+++ DummyPod/DummyClass.swift
@@ -0,0 +1,26 @@
+//
+// Created by Ramirez, Luis Manuel on 09/03/21.
+//
+
+import Foundation
+
+enum DummyClassError: Error {
+    case NotImplementedError
+    case CapacityIsUnavailable
+}
+
+class DummyClass {
+    public func getAvailableDiskSpace() -> Result<Int, Error> {
+        let fileURL = URL(fileURLWithPath: NSHomeDirectory() as String)
+        do {
+            let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
+            if let capacity = values.volumeAvailableCapacityForImportantUsage {
+                return .success(Int(capacity))
+            } else {
+                return .failure(DummyClassError.CapacityIsUnavailable)
+            }
+        } catch {
+            return .failure(error)
+        }
+    }
+}
